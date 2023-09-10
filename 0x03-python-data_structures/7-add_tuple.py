6f033932afde5de1fa90e4d95dc43796bf951de1
othmanoach <othmanoach@gmail.com>
2023-09-10 12:30:54 +0100

    add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a += (0, 0)
-    tuple_b += (0, 0)
-    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    def extract(x: tuple, i: int) -> int:
+        return x[i] if len(x) >= i + 1 else 0
+
+    return (extract(tuple_a, 0) + extract(tuple_b, 0),
+            extract(tuple_a, 1) + extract(tuple_b, 1))
