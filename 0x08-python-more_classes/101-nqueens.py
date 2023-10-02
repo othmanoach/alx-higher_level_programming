5b9731d21aa63c33d7765eec8d091f2126afe9ef
othmanoach <othmanoach@gmail.com>
2023-10-02 22:27:07 +0100

    fix

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,71 +1,35 @@
 #!/usr/bin/python3
 import sys
 
-global N
-N = 4
- 
- 
-def printSolution(board):
-    for i in range(N):
-        for j in range(N):
-            if board[i][j] == 1:
-                print("Q",end=" ")
-            else:
-                print(".",end=" ")
-        print()
- 
- 
-def isSafe(board, row, col):
- 
-    for i in range(col):
-        if board[row][i] == 1:
+def is_safe(queen, queens):
+    for (row, col) in queens:
+        if row == queen[0] or col == queen[1]:
             return False
- 
-    for i, j in zip(range(row, -1, -1),
-                    range(col, -1, -1)):
-        if board[i][j] == 1:
+        if (queen[0] - row) == (queen[1] - col) or (queen[0] - row) == (col - queen[1]):
             return False
- 
-    for i, j in zip(range(row, N, 1),
-                    range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
- 
     return True
- 
- 
-def solveNQUtil(board, col):
- 
 
-    if col >= N:
-        return True
- 
-    for i in range(N):
- 
-        if isSafe(board, i, col):
- 
-            board[i][col] = 1
- 
-            if solveNQUtil(board, col + 1) == True:
-                return True
- 
-            board[i][col] = 0
- 
-    return False
- 
-def solveNQ():
-    board = [[0, 0, 0, 0],
-             [0, 0, 0, 0],
-             [0, 0, 0, 0],
-             [0, 0, 0, 0]]
- 
-    if solveNQUtil(board, 0) == False:
-        print("Solution does not exist")
-        return False
- 
-    printSolution(board)
-    return True
- 
- 
-if __name__ == '__main__':
-    solveNQ()
+def n_queens(n, row=0, queens=[]):
+    if row == n:
+        print(queens)
+        return
+    for col in range(n):
+        queen = [row, col]
+        if is_safe(queen, queens):
+            queens.append(queen)
+            n_queens(n, row + 1, queens)
+            queens.pop()
+
+if name == "main":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    n_queens(N)
