ec2019875523957925fa6c3cdbb05c137bf38943
othmanoach <othmanoach@gmail.com>
2023-10-02 22:42:15 +0100

    queen

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,41 +1,30 @@
+#!/usr/bin/python3
+import sys
+
 def is_safe(board, row, col, n):
     """Check if it's safe to place a queen at board[row][col]."""
-    
-    # Check the column
     for i in range(row):
         if board[i][col] == 1:
             return False
-
-    # Check upper left diagonal
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-
-    # Check upper right diagonal
     for i, j in zip(range(row, -1, -1), range(col, n)):
         if board[i][j] == 1:
             return False
-
     return True
-#!/usr/bin/python3
 
 def solve_nqueens(board, row, n, solutions):
-    """Utilize backtracking to solve the N-Queens puzzle."""
-    
     if row == n:
-        # Found a solution
         solutions.append(board_to_result(board, n))
         return
-
     for col in range(n):
         if is_safe(board, row, col, n):
             board[row][col] = 1
             solve_nqueens(board, row + 1, n, solutions)
-            # Backtrack
             board[row][col] = 0
 
 def board_to_result(board, n):
-    """Convert the board to result format."""
     result = []
     for i in range(n):
         for j in range(n):
@@ -44,14 +33,20 @@ def board_to_result(board, n):
                 break
     return result
 
-def n_queens(N):
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    N = int(sys.argv[1])
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
     board = [[0 for _ in range(N)] for _ in range(N)]
     solutions = []
     solve_nqueens(board, 0, N, solutions)
-    return solutions
-
-# Example usage:
-N = 4
-solutions = n_queens(N)
-for sol in solutions:
-    print(sol)
+    for sol in solutions:
+        print(sol)
