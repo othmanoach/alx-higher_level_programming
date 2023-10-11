dfb6d010904fc9f7078eeafb4dc1223133bc3520
othmanoach <othmanoach@gmail.com>
2023-10-11 03:35:17 +0100

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,7 +11,7 @@ import os.path
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-my_file = 'add_item.json'
+my_file = 'add_item'
 
 my_list = []
 
