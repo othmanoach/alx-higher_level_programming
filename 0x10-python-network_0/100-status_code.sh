598694c50821b340aaa99d2daa5c1da2932f4b03
othmanoach <othmanoach@gmail.com>
2024-01-05 20:04:30 +0100

    fix

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that sends a request to a URL passed as an argument
-curl -sI -w '%{response_code}' "$1" -o /dev/null
+# Sends a request to a URL passed as an argument, and displays only the status code
+curl -s -o /dev/null -w "%{http_code}" "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in URL and display all HTTP methods
-curl -sI "$1" | grep "Allow: " | cut -d' ' -f 2-
+# Script that takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep -i "Allow" | awk -F ": " '{ print $2 }'
